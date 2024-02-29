72140451b424dd9184bbdf3a3ccd7e4192f7b8e6
Marcella Correia <110602658+Marcellacr@users.noreply.github.com>
2024-02-29 16:16:08 +0000

    Add files via upload

---
+++ Parte3/01_array/scripts.js
@@ -0,0 +1,9 @@
+let arr = [1,4,5,6,7];
+let nomes = ["Marcella", "Romário", "Mãe"];
+let bool = [true, false,true];
+let misturado = [1, "Marcella", true];
+
+console.log(arr);
+console.log(nomes);
+console.log(bool);
+console.log(misturado);
---
+++ Parte3/02_mais_sobre_array/scripts.js
@@ -0,0 +1,10 @@
+let arr = [1,4,5,6,7];
+let nomes = ["Marcella", "Romário", "Mãe"];
+let bool = [true, false,true];
+
+console.log(arr[1]);
+console.log(nomes[0]);
+console.log(bool[2]);
+
+//codigo para saber o ultimo elemento do array
+console.log(arr[arr.length - 1]);
